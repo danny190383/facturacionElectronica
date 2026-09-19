552c23d9dbaefa3971be34846d8038002dc01f3e
PROYECTOISSFA <PROYECTOISSFA@DESKTOP-EDTOSJ3.local>
2026-09-19 11:58:48 -0500

    19 09  2026

--- FactuNet-vet/src/main/java/com/jvc/factunet/movil/bean/PedidoVentaMovilBean.java
+++ FactuNet-vet/src/main/java/com/jvc/factunet/movil/bean/PedidoVentaMovilBean.java
@@ -1040,8 +1040,8 @@ public class PedidoVentaMovilBean extends CatalogosPersonaMovilBean implements S
         for(int i = cantString.length() ; i<=4 ; i++){
             cantString = cantString +temp[0];
         }
-         if(nombre.length() > 25){ 
-            nombre = nombre.substring(0, 25); 
+         if(nombre.length() > 30){ 
+            nombre = nombre.substring(0, 30); 
         }
         return cantString+nombre+ "\n" +temp[0]+temp[0]+temp[0]+temp[0]+temp[0]+temp[0]+descripcion;
     }
